5dc4252d076e18ea98b6afdc8d3b966906faac14
Bach Nghia Nguyen <bachnghianguyen@gmail.com>
2022-04-04 14:49:41 +0700

    Create context and render context transaction data to UI

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "context/GlobalState";
+import TransactionListItem from "components/TransactionListItem";
 
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  console.log("transactions", transactions);
+
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>-$400</span> <button className="delete-btn">x</button>
-        </li>
+        {transactions.map((transaction) => (
+          <TransactionListItem key={transaction.id} transaction={transaction} />
+        ))}
       </ul>
     </>
   );
